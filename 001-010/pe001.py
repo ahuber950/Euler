dc9c3b7e75e541958bd3ff8692f0307a7d9f55b0
ahuber950 <ahuber950@gmail.com>
2020-10-02 19:59:35 -0400

    added mod1 unit test

--- 001-010/pe001.py
+++ 001-010/pe001.py
@@ -3,16 +3,17 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-import time
+def mod1(lim):
 
-st = time.time()
-
-sum = 0
-for i in range(1, 1000):
-    if i % 3 == 0 or i % 5 == 0:
-        sum += i
-print(sum)
-
-end = time.time()
-
-print("this code took ", str(end - st), " seconds to run")
+	if type(lim) != int:
+		raise ValueError("The limit must be a non-negative integer")
+	
+	if lim < 0:
+		raise ValueError("The limit must be a non-negative integer")
+	
+	sum = 0	
+	for i in range(1, lim):
+	    if i % 3 == 0 or i % 5 == 0:
+	        sum += i
+	
+	return(sum)
