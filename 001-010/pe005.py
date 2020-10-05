c923623e7d872e41ee6ba6cf196225ec6f97efbe
ahuber950 <ahuber950@gmail.com>
2020-10-05 16:03:27 -0400

    added unittest to pe005 and deleted time stamp code for pe005

--- 001-010/pe005.py
+++ 001-010/pe005.py
@@ -4,10 +4,6 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 # define function for listing primary factors
-import math
-import time
-
-st = time.time()
 
 
 def pfacs(num):
@@ -28,23 +24,19 @@ def prod(list):
     return p
 
 
-x = int(
-    input(
-        "I want the smallest number that is evenly divisible by all numbers from 1 to __\n"
-    )
-)
-min_facs = []
-for i in range(2, x + 1):
-    facs_facs = pfacs(i)
-    for j in facs_facs:
-        while facs_facs.count(j) > min_facs.count(j):
-            min_facs.append(j)
-            min_facs.sort()
-            print(str(min_facs) + " appended " + str(j) + " on i " + str(i))
+def mod5(num):
 
-print(min_facs)
-p = prod(min_facs)
-print(p)
+    if type(num) != int or num < 1:
+        raise ValueError("The value must be an integer greater than zero")
 
-end = time.time()
-print(str(end - st) + " s")
+
+    min_facs = []
+    for i in range(2, num + 1):
+        facs_facs = pfacs(i)
+        for j in facs_facs:
+            while facs_facs.count(j) > min_facs.count(j):
+                min_facs.append(j)
+                min_facs.sort()
+
+    p = prod(min_facs)
+    return(p)
