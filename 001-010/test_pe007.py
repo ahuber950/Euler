e3cf51b13161abd02d60b118547da2de986814fd
ahuber950 <ahuber950@gmail.com>
2020-10-08 10:38:09 -0400

    adding unittest to pe007

--- 001-010/test_pe007.py
+++ 001-010/test_pe007.py
@@ -6,6 +6,7 @@ class Testpe007(unittest.TestCase):
     def test_pe007(self):
         self.assertEqual(mod7(6), 13)
         self.assertEqual(mod7(10001), 104743)
+        self.assertEqual(mod7(1), 2)
 
     def test_values(self):
         self.assertRaises(ValueError, mod7, -1)
