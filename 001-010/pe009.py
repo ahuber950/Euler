bc05df5e8454497d7db3f8a5bbc5bce7762ae2ef
ahuber950 <ahuber950@gmail.com>
2020-10-08 12:55:17 -0400

    removed time stamps and eliminated case where side length could be equal to zero

--- 001-010/pe009.py
+++ 001-010/pe009.py
@@ -3,22 +3,23 @@
 # A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
 # a2 + b2 = c2
-# For example, 32 + 42 = 9 + 16 = 25 = 52.
+# For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**2.
 
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
-import time
 
-st = time.time()
+def mod9(desired_sum):
 
-for c in range(501):
-    for b in range(251):
-        a = 1000 - c - b
-        if a ** 2 + b ** 2 == c ** 2:
-            print(a, b, c, a * b * c)
+	import math
 
-            quit()
+	if type(desired_sum) != int or desired_sum < 1:
+		raise ValueError("The value must be an integer greater than zero")
 
-end = time.time()
-print("this code took ", str(end - st), " seconds to run")
+	for c in range(1, math.ceil((desired_sum / 2 + 1))):
+		for b in range(1, math.ceil((desired_sum / 4 + 1))):
+			a = desired_sum - c - b
+			if a ** 2 + b ** 2 == c ** 2 and b != c and a != c:
+				return a * b * c
+
+	return('No Pythagorean Triplet exists with that sum')
